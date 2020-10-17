c3a9ae2926ccc8caa57aeabda04c58c97eeea58d
Avinda Abeywardana <avinda.abeywardana@shoppa.com>
2020-10-17 17:07:49 +0530

    Add command center component to the home page

---
+++ src/components/CommandCenter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Command Center</h2>
+    <h3>Bullets: {{ bullets }}</h3>
+    <h4>Enough For: {{ timeNeedForReSupply }} hours</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bullets: 200000,
+    };
+  },
+  computed: {
+    timeNeedForReSupply() {
+      return Math.round(this.bullets / 2 / 60 / 60);
+    },
+  },
+  methods: {
+    sendSupply() {},
+  },
+};
+</script>
+
+<style lang="sass" scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <CommandCenter></CommandCenter>
+  </div>
+</template>
+
+<script>
+import CommandCenter from '@/components/CommandCenter.vue';
+
+export default {
+  components: {
+    CommandCenter
+  },
+};
+
+</script>
+
+<style lang="sass" scoped>
+
+</style>
